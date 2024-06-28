8fb328c034408ba2771013bcd86f6524e92d559c
CeciliaMon <dreamynylle17@gmail.com>
2024-06-28 17:52:18 -0300

    arreglando error
    arreglando error

--- backend/controllers/localController.js
+++ backend/controllers/localController.js
@@ -8,16 +8,18 @@ localCtrl.getlocales = async (req, res) => {
 
 localCtrl.createlocal = async (req, res) => {
     var local = new Local(req.body);
-        try {
-            await local.save();
-            res.status(200).json({
+    try {
+        await local.save();
+        res.status(200).json({
             'status': '1',
-            'msg': 'Local guardado.'})
-        } catch (error) {
-            res.status(400).json({
+            'msg': 'Local guardado.'
+        })
+    } catch (error) {
+        res.status(400).json({
             'status': '0',
-            'msg': 'Error procesando operacion.'})
-        }
+            'msg': 'Error procesando operacion.'
+        })
+    }
 }
 
 localCtrl.getLocal = async (req, res) => {
@@ -27,39 +29,40 @@ localCtrl.getLocal = async (req, res) => {
 
 localCtrl.editLocal = async (req, res) => {
     const vlocal = new Local(req.body);
-        try {
-            await Local.updateOne({_id: req.body._id}, vlocal);
-            res.status(200).json({
+    try {
+        await Local.updateOne({ _id: req.body._id }, vlocal);
+        res.status(200).json({
             'status': '1',
             'msg': 'Local editado'
-            })
-        } catch (error) {
-            res.status(400).json({
-            'status': '0',
-            'msg': 'Error procesando la operacion editar'
-            })
-        }
-}
-
-localCtrl.deleteLocal = async (req, res)=>{
-    try {
-        await Local.deleteOne({_id: req.params.id});
-        res.json({
-        status: '1',
-        msg: 'local removed'
         })
     } catch (error) {
         res.status(400).json({
-        'status': '0',
-        'msg': 'Error procesando la operacion delete'
+            'status': '0',
+            'msg': 'Error procesando la operacion editar'
+        })
+    }
+}
+
+localCtrl.deleteLocal = async (req, res) => {
+    try {
+        await Local.deleteOne({ _id: req.params.id });
+        res.json({
+            status: '1',
+            msg: 'local removed'
+        })
+    } catch (error) {
+        res.status(400).json({
+            'status': '0',
+            'msg': 'Error procesando la operacion delete'
         });
     }
 
-    localCtrl.getLocalesNoAlquilados = async (req, res) => {
-        var locales = await Local.find({alquilado : false});
-        res.json(locales);
-    };
+    
 }
+localCtrl.getLocalesNoAlquilados = async (req, res) => {
+    var locales = await Local.find({ alquilado: false });
+    res.json(locales);
+};
 
 
 module.exports = localCtrl;
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const {mongoose} = require('./database');
+const {mongoose} = require('./database.js');
 var app = express();
 
 //middlewares
