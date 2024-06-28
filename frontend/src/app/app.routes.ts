61e7208dca78e572f8131b96889df0254a438c0b
MaxiFlores717 <103212606+MaxiFlores717@users.noreply.github.com>
2024-06-28 14:22:45 -0300

    header and footer
    header and footer

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { AlquilerComponent } from './components/alquiler/alquiler.component';
 
 export const routes: Routes = [
-    
+    {
+        path: 'alquiler',
+        component: AlquilerComponent,
+    }
 ];
---
+++ frontend/src/app/components/alquiler/alquiler.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alquiler',
+  standalone: true,
+  imports: [],
+  templateUrl: './alquiler.component.html',
+  styleUrl: './alquiler.component.css'
+})
+export class AlquilerComponent {
+
+}
