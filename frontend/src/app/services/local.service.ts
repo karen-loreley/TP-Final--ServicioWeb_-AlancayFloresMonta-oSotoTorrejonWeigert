c54772c2fc16648d616bba4ef9a3bb00a3231cb0
leonardo20041 <leosoto2004@gmail.com>
2024-06-28 23:38:42 -0300

    Punto 5
    Local y Local-Form

--- frontend/src/app/services/local.service.ts
+++ frontend/src/app/services/local.service.ts
@@ -1,22 +1,82 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Local } from '../models/local';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalService {
-  urlBase: string = 'http://localhost:3000/api/local/';
+
+  listaLocales!: Array<Local>;
+
   constructor(private _http: HttpClient) { }
 
-  getLocalesNoAlquilados(): Observable<any>{
-    let httpOption = {
-      headers: new HttpHeaders({
-
-      }),
-      params: new HttpParams()
+  addLocal(local: Local): Observable<any>
+  {
+    console.log(local);
+    let httpOption = 
+    {
+      headers: new HttpHeaders
+      ({
+        'Content-Type': 'application/json'
+      })
     }
 
-    return this._http.get(this.urlBase + 'noAlquilados', httpOption);
+    let body: any = JSON.stringify(local);
+    return this._http.post("http://localhost:3000/api/local", body, httpOption);
   }
+
+  getLocales(): Observable<any>
+  {
+    let httpOption = 
+    {
+      headers: new HttpHeaders
+      ({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this._http.get("http://localhost:3000/api/local", httpOption);
+  }
+
+  getLocalById(_id: string): Observable<any>
+  {
+    let httpOption = 
+    {
+      headers: new HttpHeaders
+      ({
+        'Content-Type': 'application/json'
+      })
+    }
+    
+    return this._http.get("http://localhost:3000/api/local/" +_id, httpOption);
+  }
+
+  updateLocal(local: Local): Observable<any> 
+  { 
+    let httpOption = 
+    {
+      headers: new HttpHeaders
+      ({
+        'Content-Type': 'application/json'
+      })
+    }
+
+    let body: any = JSON.stringify(local);
+    return this._http.put("http://localhost:3000/api/local/" + local._id, body, httpOption);
+  }
+
+  deleteLocal(_id: string): Observable<any>
+  {
+    let httpOption = 
+    {
+      headers: new HttpHeaders
+      ({
+        'Content-Type': 'application/json'
+      })
+    }
+
+    return this._http.delete(`http://localhost:3000/api/local/${_id}`, httpOption);
+  }
+  
 }
