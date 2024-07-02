1c09d8de22199803b482bfb562a148e47ecd20e1
karen-loreley <karen.loreley.weigert@gmail.com>
2024-07-01 22:36:39 -0300

    creacion del login en el frontend
    creacion del login en el frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,8 +4,13 @@ import { Component } from '@angular/core';
 import { CrudUsuariosComponent } from './components/crud-usuarios/crud-usuarios.component';
 import { UsuarioFormComponent } from './components/usuario-form/usuario-form.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+    },
     {
         path: 'home', component: HomeComponent
 
