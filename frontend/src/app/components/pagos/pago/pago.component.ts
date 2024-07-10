0c8ba4b542990ecfa87812c2c20b99bb9d8fb029
MaxiFlores717 <103212606+MaxiFlores717@users.noreply.github.com>
2024-07-10 04:07:17 -0300

    correcciones a punto8

--- frontend/src/app/components/pagos/pago/pago.component.ts
+++ frontend/src/app/components/pagos/pago/pago.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { PagoService } from '../../../services/pago.service';
 import { Router } from '@angular/router';
@@ -26,49 +26,26 @@ import { MercadoPagoConfig, Preference } from 'mercadopago';
   styleUrl: './pago.component.css'
 })
 export class PagoComponent {
-
+  
   listaPagos!: Array<Pago>;
   listaLocal!: Array<Local>;
+  graficoPagosMensualesVisible: boolean = false;
+  localVisible: boolean = false;
 
+  
+  totalPorMes: number[] = new Array(12).fill(0);
+  nombresLocales: string[] = [];
 
-  enero: number = 0;
-  febrero: number = 0;
-  marzo: number = 0;
-  abril: number = 0;
-  mayo: number = 0;
-  junio: number = 0;
-  julio: number = 0;
-  agosto: number = 0;
-  septiembre: number = 0;
-  octubre: number = 0;
-  noviembre: number = 0;
-  diciembre: number = 0;
+  conteoPagosPorLocal: { [nombre: string]: number } = {};
 
-  totalEnero: number = 0;
-  totalFebrero: number = 0;
-  totalMarzo: number = 0;
-  totalAbril: number = 0;
-  totalMayo: number = 0;
-  totalJunio: number = 0;
-  totalJulio: number = 0;
-  totalAgosto: number = 0;
-  totalSeptiembre: number = 0;
-  totalOctubre: number = 0;
-  totalNoviembre: number = 0;
-  totalDiciembre: number = 0;
-
-
-
-
-  local1: number = 0;
-  local2: number = 0;
-
+  currentPage: number = 1;
 
   constructor(private pagoService: PagoService,
     private localService: LocalService,
     private router: Router) {
+    this.obtenerLocales(this.currentPage);
+
     this.obtenerPagos();
-    this.obtenerLocales();
   }
 
   ngOnInit(): void {
@@ -108,100 +85,45 @@ export class PagoComponent {
         this.listaPagos = data;
         console.log(this.listaPagos);
 
-        const meses = this.listaPagos.map(pago => new Date(pago.mes).getMonth());
-
-        console.log(meses);
-
         this.listaPagos.forEach(pago => {
           const mes = new Date(pago.mes).getMonth();
-
-          if (mes == 0) {
-            this.enero++;
-            this.totalEnero += pago.local.costomes;
-          }
-          if (mes == 1) {
-            this.febrero++;
-            this.totalFebrero += pago.local.costomes;
-
-          }
-          if (mes == 2) {
-            this.marzo++;
-            this.totalMarzo += pago.local.costomes;
-
-          }
-          if (mes == 3) {
-            this.abril++;
-            this.totalAbril += pago.local.costomes;
-            
-
-          }
-          if (mes == 4) {
-            this.mayo++;
-            this.totalMayo += pago.local.costomes;
-
-          }
-          if (mes == 5) {
-            this.junio++;
-            this.totalJunio += pago.local.costomes;
-
-          }
-          if (mes == 6) {
-            this.julio++;
-            this.totalJulio += pago.local.costomes;
-
-          }
-          if (mes == 7) {
-            this.agosto++;
-            this.totalAgosto += pago.local.costomes;
-
-          }
-          if (mes == 8) {
-            this.septiembre++;
-            this.totalSeptiembre += pago.local.costomes;
-
-          }
-          if (mes == 9) {
-            this.octubre++;
-            this.totalOctubre += pago.local.costomes;
-
-          }
-          if (mes == 10) {
-            this.noviembre++;
-            this.totalNoviembre += pago.local.costomes;
-
-          }
-          if (mes == 11) {
-            this.diciembre++;
-            this.totalDiciembre += pago.local.costomes;
-
-          }
-
+          this.totalPorMes[mes] += pago.local.costomes;
+        });
+        
+        // Inicializar conteo para cada local
+        this.listaLocal.forEach(local => {
+          this.conteoPagosPorLocal[local.nombre] = 0;
         });
 
-      },
-      error => {
-        console.log(error);
-      }
-
-    )
-
-  }
-  obtenerLocales() {
-    this.pagoService.getPagos().subscribe(
-      data => {
-        this.listaPagos = data;
-        console.log(this.listaPagos);
-
-
+        // Contar los pagos por local
         this.listaPagos.forEach(pago => {
-          const nombre = pago.local.nombre.toString();
-          if (nombre === "Local 1") {
-            this.local1++;
-          }
-          if (nombre === "Local 2") {
-            this.local2++;
+          const nombreLocal = pago.local.nombre;
+          if (this.conteoPagosPorLocal.hasOwnProperty(nombreLocal)) {
+            this.conteoPagosPorLocal[nombreLocal] += pago.local.costomes;
           }
         });
+
+        console.log('Conteo de pagos por local:', this.conteoPagosPorLocal);
+
+      },
+      error => {
+        console.log(error);
+      }
+
+    )
+
+  }
+  obtenerLocales(page: number) {
+    this.localService.getLocales(page, 6).subscribe(
+      data => {
+        this.listaLocal = data.locales;
+
+
+        this.listaLocal.forEach(local => {
+          this.nombresLocales.push(local.nombre);
+        });
+
+        console.log(this.nombresLocales);
 
       },
       error => {
@@ -210,10 +132,10 @@ export class PagoComponent {
 
     )
   }
+  
+
   dibujarGrafico() {
-    const meses = this.listaPagos.map((pago) => { pago.mes });
-
-
+    
 
     const ctx = document.getElementById('graficoPagosMensuales') as HTMLCanvasElement;
     if (!ctx) {
@@ -227,7 +149,7 @@ export class PagoComponent {
         labels: ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'],
         datasets: [{
           label: 'Pagos realizados',
-          data: [this.totalEnero, this.totalFebrero, this.totalMarzo, this.totalAbril, this.totalMayo, this.totalJunio, this.totalJulio, this.totalAgosto, this.totalSeptiembre, this.totalOctubre, this.totalNoviembre, this.totalDiciembre],
+          data: this.totalPorMes,
           backgroundColor: [
             'rgba(75, 192, 192, 0.2)',    // Color para la primera barra
             'rgba(255, 99, 132, 0.2)',    // Color para la segunda barra
@@ -262,7 +184,6 @@ export class PagoComponent {
   }
   filtrarLocal() {
 
-
     const ctx = document.getElementById('local') as HTMLCanvasElement;
     if (!ctx) {
       console.error('No se encontró el elemento canvas con id "graficoPagosMensuales"');
@@ -272,10 +193,10 @@ export class PagoComponent {
     new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: ['local 1', 'local2'],
+        labels: this.nombresLocales,
         datasets: [{
-          label: 'Pagos realizados por Local',
-          data: [this.local1, this.local2],
+          label: 'Total recaudado por Local',
+          data: this.conteoPagosPorLocal,
           backgroundColor: [
             'rgba(75, 192, 192, 0.2)',    // Color para la primera barra
             'rgba(255, 99, 132, 0.2)',    // Color para la segunda barra
@@ -298,8 +219,10 @@ export class PagoComponent {
         scales: {
           y: {
             beginAtZero: true,
-            max: 7 // Establece el valor máximo
-
+            max: 1000, // Cambia el valor máximo a un número más grande
+            ticks: {
+              stepSize: 100 // Ajusta el tamaño del paso entre las marcas de graduación
+            }
           }
         }
       }
@@ -328,9 +251,6 @@ export class PagoComponent {
      )*/
   }
 
-  agregarTicket() {
-    this.router.navigate(['pago-form', 0]);
-  }
   /*
     modificarTicket(id: string | undefined) {
       console.log(id);
